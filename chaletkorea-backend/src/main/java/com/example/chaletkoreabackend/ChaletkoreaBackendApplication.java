9c4e59a65d2bc8eb5eb2b75c4ddcc2a29cdfdf38
HsKim4054 <gudtjq444@naver.com>
2024-08-07 14:04:36 +0900

    초기 설정
    초기 설정

---
+++ chaletkorea-backend/src/main/java/com/example/chaletkoreabackend/ChaletkoreaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chaletkoreabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaletkoreaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaletkoreaBackendApplication.class, args);
+    }
+
+}
---
+++ chaletkorea-backend/src/test/java/com/example/chaletkoreabackend/ChaletkoreaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chaletkoreabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaletkoreaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

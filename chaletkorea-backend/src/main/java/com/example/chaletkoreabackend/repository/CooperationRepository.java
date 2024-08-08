c48219835616ec6ac86d617f9992099e3bb3fcc5
minjpark3 <minj9850610@gmail.com>
2024-08-08 15:35:47 +0900

    fix

---
+++ chaletkorea-backend/src/main/java/com/example/chaletkoreabackend/repository/CooperationRepository.java
@@ -0,0 +1,7 @@
+package com.example.chaletkoreabackend.repository;
+
+import com.example.chaletkoreabackend.entity.cooperation.Cooperation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CooperationRepository extends JpaRepository<Cooperation, Long> {
+}
